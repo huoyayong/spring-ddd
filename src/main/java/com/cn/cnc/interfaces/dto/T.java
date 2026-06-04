5136b8f74278884c68abac2fabce32c0246113b7
alex.huo <413261403@qq.com>
2026-06-04 21:30:06 +0800

    feat: 测试

--- src/main/java/com/cn/cnc/interfaces/dto/T.java
+++ src/main/java/com/cn/cnc/interfaces/dto/T.java
@@ -1,9 +1,18 @@
 package com.cn.cnc.interfaces.dto;
 
+import org.HdrHistogram.HistogramLogReader;
+
 /**
  * Created by EalenXie on 2018/8/30 18:10.
  */
+
 public class T {
 
     private String ss;
+
+    public static final String password = "aaaa";
+
+    public String s(String args, HistogramLogReader reader) {
+        return "";
+    }
 }
